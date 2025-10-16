88739c5428d737b52683d90006285aa6e4861889
Dnts77 <daniel.souzadantas07@gmail.com>
2025-10-16 16:39:39 -0300

    feat: Adição das primeiras funcionalidades da 2° interface (obs: ainda é necessário adicionar as funcionalidades de cadastro).

---
+++ app/src/main/java/com/example/diabfit/Cadastro.java
@@ -0,0 +1,52 @@
+package com.example.diabfit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Cadastro extends AppCompatActivity implements View.OnClickListener {
+
+    Button btCADCadastro, btVoltar;
+    EditText txtCADNome, txtCADEmail, txtCADSenha, txtCADCONFSenha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Id's da página de cadastro
+        btCADCadastro = findViewById(R.id.btCADCadastro);
+        btVoltar = findViewById(R.id.BtVoltar);
+        txtCADCONFSenha = findViewById(R.id.txtCADCONFSenha);
+        txtCADEmail = findViewById(R.id.txtCADEmail);
+        txtCADNome = findViewById(R.id.txtCADNome);
+        txtCADSenha = findViewById(R.id.txtCADSenha);
+
+        btVoltar.setOnClickListener(this);
+        btCADCadastro.setOnClickListener(this);
+    }
+
+
+    //função que faz o botão de Voltar funcionar
+    @Override
+    public void onClick(View v) {
+        if (v .getId()== R.id.BtVoltar){
+            Intent intent = new Intent(Cadastro.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
