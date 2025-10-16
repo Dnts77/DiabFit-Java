b353236f6fc4037768e2310dbf2227f9e822f978
Dnts77 <daniel.souzadantas07@gmail.com>
2025-10-16 16:40:38 -0300

    fix: Primeira versão do banco de dados do app (obs: Testar se o código roda após a função de cadastro ser implementada).

--- app/src/main/java/com/example/diabfit/AppDatabase.java
+++ app/src/main/java/com/example/diabfit/AppDatabase.java
@@ -4,20 +4,27 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 public class AppDatabase extends SQLiteOpenHelper {
-    public AppDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private static final String NOME_BANCO = "diabfit_db";
+    private static final int VERSAO = 2;
+    public AppDatabase(Context context) {
+        super(context, NOME_BANCO, null, VERSAO);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String sql = "CREATE TABLE cadastros ("
+                + "senha integer,"
+                + "nome text,"
+                + "email text)";
+        db.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS cadastros");
+        onCreate(db);
     }
 }
+
