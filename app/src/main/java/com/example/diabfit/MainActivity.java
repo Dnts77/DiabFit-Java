0e5e35e020a56d079ee617d22bd97ee0e459a2c6
Dnts77 <daniel.souzadantas07@gmail.com>
2025-10-23 15:16:44 -0300

    feat: Adicionando feature que solicita os campos preenchidos e só permite entrar caso as informações estejam no bd

--- app/src/main/java/com/example/diabfit/MainActivity.java
+++ app/src/main/java/com/example/diabfit/MainActivity.java
@@ -1,52 +1,78 @@
 package com.example.diabfit;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText txtNome, txtEmail, txtSenha;
+    EditText txtEmail, txtSenha;
     Button btCadastro, btEntrar;
+    private UserDAO userDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         btCadastro = findViewById(R.id.btCadastro);
         btEntrar = findViewById(R.id.btEntrar);
         txtEmail = findViewById(R.id.txtEmail);
-        txtNome = findViewById(R.id.txtNome);
         txtSenha = findViewById(R.id.txtSenha);
 
         btCadastro.setOnClickListener(this);
         btEntrar.setOnClickListener(this);
 
-    }
 
+        userDAO = new UserDAO(this);
+    }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btCadastro) {
             Intent intent = new Intent(MainActivity.this, Cadastro.class);
             startActivity(intent);
+        } else if (v.getId() == R.id.btEntrar) {
+            tentarLogin();
+        }
+    }
+
+    private void tentarLogin() {
+        String email = txtEmail.getText().toString().trim();
+        String senha = txtSenha.getText().toString().trim();
+
+
+        if (TextUtils.isEmpty(email)) {
+            txtEmail.setError("Este campo é obrigatório");
+            txtEmail.requestFocus();
+            return;
         }
 
+        if (TextUtils.isEmpty(senha)) {
+            txtSenha.setError("Este campo é obrigatório");
+            txtSenha.requestFocus();
+            return;
+        }
+
+
+        userDAO.open();
+        boolean loginValido = userDAO.checarUsuario(email, senha);
+        userDAO.close();
+
+
+        if (loginValido) {
+            Toast.makeText(this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(this, "E-mail ou senha incorretos.", Toast.LENGTH_LONG).show();
+            txtSenha.setError("Credenciais inválidas");
+            txtSenha.requestFocus();
+        }
     }
 }
