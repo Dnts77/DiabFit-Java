873f0b2de1f1ee4107d7841ac4e17dd119224134
Dnts77 <daniel.souzadantas07@gmail.com>
2025-10-24 15:14:50 -0300

    feat: Códigos conclúidos e Cadastro.java funcionando.

--- app/src/main/java/com/example/diabfit/Cadastro.java
+++ app/src/main/java/com/example/diabfit/Cadastro.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +18,7 @@ public class Cadastro extends AppCompatActivity implements View.OnClickListener 
     Button btCADCadastro, btVoltar;
     EditText txtCADNome, txtCADEmail, txtCADSenha, txtCADCONFSenha;
     private UserDAO userDAO;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +41,53 @@ public class Cadastro extends AppCompatActivity implements View.OnClickListener 
 
         btVoltar.setOnClickListener(this);
         btCADCadastro.setOnClickListener(this);
+
+        userDAO = new UserDAO(this);
     }
 
 
     //função que faz o botão de Voltar funcionar
     @Override
     public void onClick(View v) {
-        if (v .getId()== R.id.BtVoltar){
+        if (v.getId() == R.id.BtVoltar) {
             Intent intent = new Intent(Cadastro.this, MainActivity.class);
             startActivity(intent);
+        } else if (v.getId() == R.id.btCADCadastro) {
+            Cadastrar();
         }
     }
-    private void Cadastro(){
+
+    public void Cadastrar() {
         String nome = txtCADNome.getText().toString().trim();
         String email = txtCADEmail.getText().toString().trim();
         String senha = txtCADSenha.getText().toString().trim();
+        String confSenha = txtCADCONFSenha.getText().toString().trim();
+
+
+        if(!senha.equals(confSenha)){
+            Toast.makeText(this, "As senhas não coincidem", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        userDAO.open();
+        boolean CadastroRealizado = userDAO.inserir(nome, email, senha);
+        userDAO.close();
+
+        if(CadastroRealizado){
+            Toast.makeText(this, "Cadastro realizado com êxito", Toast.LENGTH_SHORT).show();
+            //Intent intent = new Intent(Cadastro.this,) //Falta fazer a outra tela pra isso aqui ter sentido
+            //startActivity(intent);
+            finish();
+
+        }
+        else{
+            Toast.makeText(this, "Erro ao realizar o cadastro", Toast.LENGTH_SHORT).show();
+
+        }
 
     }
 }
