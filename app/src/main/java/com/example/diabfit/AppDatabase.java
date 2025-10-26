69ca06cb882e2b241d4e87776606e7a60006b50e
Dnts77 <daniel.souzadantas07@gmail.com>
2025-10-26 12:41:33 -0300

    feat: Criação da nova tabela com informações sobre o usuário

--- app/src/main/java/com/example/diabfit/AppDatabase.java
+++ app/src/main/java/com/example/diabfit/AppDatabase.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class AppDatabase extends SQLiteOpenHelper {
 
     private static final String NOME_BANCO = "diabfit_db";
-    private static final int VERSAO = 2;
+    private static final int VERSAO = 4;
     public AppDatabase(Context context) {
         super(context, NOME_BANCO, null, VERSAO);
     }
@@ -19,11 +19,18 @@ public class AppDatabase extends SQLiteOpenHelper {
                 + "nome text,"
                 + "email text)";
         db.execSQL(sql);
+        sql = "CREATE TABLE dados("
+                + "sugarLevel integer,"
+                + "peso integer,"
+                + "altura integer)";
+        db.execSQL(sql);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS cadastros");
+        db.execSQL("DROP TABLE IF EXISTS dados");
         onCreate(db);
     }
 }
