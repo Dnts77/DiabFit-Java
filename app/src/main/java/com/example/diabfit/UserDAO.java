b54b53ab6fc42ca5b13748b4358e5c729b2b8bb3
Dnts77 <daniel.souzadantas07@gmail.com>
2025-10-26 12:41:05 -0300

    feat: Novo método que insere informações do usuário

--- app/src/main/java/com/example/diabfit/UserDAO.java
+++ app/src/main/java/com/example/diabfit/UserDAO.java
@@ -35,6 +35,22 @@ public class UserDAO {
         return resultado != -1;
     }
 
+    // Método para inserir as informações do usuário (na tela de informações)
+    public boolean infos(String sugarLevel, String peso, String altura){
+        ContentValues values = new ContentValues();
+        values.put("sugarLevel", sugarLevel);
+        values.put("peso", peso);
+        values.put("altura", altura);
+        long resultado = -1;
+        try{
+            resultado = database.insert("dados", null, values);
+        } catch(Exception e){
+            Log.e("UserDAO", "Erro ao inserir informações", e);
+        }
+        return resultado != -1;
+    }
+
+
     public String getSenhaPorEmail(String email) {
         String[] columns = {"senha"};
         String selection = "email = ?";
