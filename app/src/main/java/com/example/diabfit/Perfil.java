ea607000f872b9ae7e19dd26f74482278d18f820
Dnts77 <daniel.souzadantas07@gmail.com>
2025-11-21 17:58:30 -0300

    feat: O usuário pode excluir sua conta

--- app/src/main/java/com/example/diabfit/Perfil.java
+++ app/src/main/java/com/example/diabfit/Perfil.java
@@ -1,15 +1,21 @@
 package com.example.diabfit;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,6 +27,7 @@ public class Perfil extends AppCompatActivity {
 
     private TextView textViewNome, textViewEmail, textViewPeso, textViewAltura;
 
+    private Button buttonExcluirConta;
 
     private UserDAO userDAO;
     private FirebaseAuth mAuth;
@@ -47,9 +54,24 @@ public class Perfil extends AppCompatActivity {
         textViewEmail = findViewById(R.id.textViewEmail);
         textViewPeso = findViewById(R.id.textViewPeso);
         textViewAltura = findViewById(R.id.textViewAltura);
+        buttonExcluirConta = findViewById(R.id.buttonExcluirConta);
 
         userDAO = new UserDAO(this);
         mAuth = FirebaseAuth.getInstance();
+
+        buttonExcluirConta.setOnClickListener(v -> {
+            new AlertDialog.Builder(this)
+                    .setTitle("Excluir Conta")
+                    .setMessage("Você tem certeza que deseja excluir sua conta? Esta ação não pode ser desfeita.")
+                    .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            excluirConta();
+                        }
+                    })
+                    .setNegativeButton("Não", null)
+                    .show();
+        });
 
 
         loadUserProfile();
@@ -92,6 +114,27 @@ public class Perfil extends AppCompatActivity {
             textViewAltura.setText(String.format(Locale.getDefault(), "%.0f cm", alturaEmMetros));
         } else {
             textViewAltura.setText("Não informada");
+        }
+    }
+
+    private void excluirConta() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            user.delete()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Perfil.this, "Conta excluída com sucesso.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Perfil.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(Perfil.this, "Falha ao excluir a conta.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
         }
     }
 
